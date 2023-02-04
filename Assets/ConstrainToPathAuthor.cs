413e6d8dd62de1429097beea3d53a0e01286e163
Daxode <daniel.andersen@unity3d.com>
2023-02-04 23:50:19 +0100

    Push to baking instead :3

--- Assets/ConstrainToPathAuthor.cs
+++ Assets/ConstrainToPathAuthor.cs
@@ -18,14 +18,25 @@ public class ConstrainToPathAuthor : MonoBehaviour
 
     class Baker : Baker<ConstrainToPathAuthor> {
         public override void Bake(ConstrainToPathAuthor auth) {
-            AddComponent<CurrentJoint>();
-            AddComponent<NotInitialized>();
             var followPoints = AddBuffer<FollowPoint>();
             if (auth.Path == null) return;
             followPoints.EnsureCapacity(auth.Path.Length);
             foreach (var point in auth.Path) {
                 followPoints.Add(point);
             }
+
+            if (followPoints.Length < 2) return;
+            // get the first two points
+            float3 pointA = followPoints[0];
+            float3 pointB = followPoints[1];
+            var physicsJoint = JointHelpers.CalculateJointBetween(pointA, pointB);
+            var jointEntity = JointHelpers.CreateJoint(this, GetEntity(), physicsJoint);
+
+            // Debug.Log("Created joint");
+            // // for (int i = 0; i < followPoints.Length - 1; i++) {
+            // //     Debug.DrawLine((float3)followPoints[i], (float3)followPoints[i + 1], Color.green, 20f);
+            // // }
+            AddComponent(new CurrentJoint { Value = jointEntity });
         }
     }
 
@@ -41,7 +52,6 @@ public class ConstrainToPathAuthor : MonoBehaviour
     }
 }
 
-struct NotInitialized : IComponentData {}
 struct CurrentJoint : IComponentData {
     public Entity Value;
 }
@@ -60,81 +70,93 @@ partial struct PathConstraintBakingSystem : ISystem {
 }
 
 
+// Create system with singleton of whether Q is pressed
+partial struct InputSystem : ISystem {
+    public struct InputState : IComponentData {
+        public FixedInputEvent Q;
+    }
+
+    public void OnCreate(ref SystemState state) {
+        state.EntityManager.AddComponentData(state.SystemHandle, new InputState());
+        state.RequireForUpdate<FixedTickSystem.Singleton>();
+    }
+
+    public void OnDestroy(ref SystemState state) {}
+
+    public void OnUpdate(ref SystemState state) {
+        var fixedTick = SystemAPI.GetSingleton<FixedTickSystem.Singleton>().Tick;
+        var inputState = state.EntityManager.GetComponentDataRW<InputState>(state.SystemHandle);
+        if (Input.GetKeyDown(KeyCode.Q))
+            inputState.ValueRW.Q.Set(fixedTick);
+    }
+}
+
+
 // Update in physics group
-[UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
 [BurstCompile]
+[UpdateInGroup(typeof(BeforePhysicsSystemGroup))]
 public partial struct PathConstraintSystem : ISystem {
-    public void OnCreate(ref SystemState state){}
+    public void OnCreate(ref SystemState state){
+        state.RequireForUpdate<FixedTickSystem.Singleton>();
+    }
     public void OnDestroy(ref SystemState state){}
 
+    int i;
     //[BurstCompile]
     public void OnUpdate(ref SystemState state) {
-        var ecb = SystemAPI.GetSingleton<EndFixedStepSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer(state.WorldUnmanaged);
-        // only run if space presssed
-        if (!Input.GetKeyDown(KeyCode.Space)) return;
-
-        foreach (var (followPoints, localTransform, entity) in SystemAPI.Query<DynamicBuffer<FollowPoint>, RefRO<LocalTransform>>().WithAll<NotInitialized>().WithEntityAccess()) {
-            // continue if follow points is less than 2
-            if (followPoints.Length < 2) continue;
-            // get the first two points
-            float3 pointA = followPoints[0];
-            float3 pointB = followPoints[1];
-            var physicsJoint = CalculateJointBetween(pointA, pointB);
-            var jointEntity = CreateJoint(ecb, entity, physicsJoint);
-
-            Debug.Log("Created joint");
-            // for (int i = 0; i < followPoints.Length - 1; i++) {
-            //     Debug.DrawLine((float3)followPoints[i], (float3)followPoints[i + 1], Color.green, 20f);
-            // }
-            ecb.SetComponent(entity, new CurrentJoint { Value = jointEntity });
-
-            ecb.RemoveComponent<NotInitialized>(entity);
-        }
-
-        foreach (var (followPoints, localTransform, currentJoint, entity) in SystemAPI.Query<DynamicBuffer<FollowPoint>, RefRO<LocalTransform>, RefRO<CurrentJoint>>().WithEntityAccess().WithNone<NotInitialized>()) {
-            // continue if follow points is less than 3
-            if (followPoints.Length < 3) continue;
-            Debug.Log("Created joint");
-            // get the first two points
-            float3 pointA = followPoints[1];
-            float3 pointB = followPoints[2];
-            var physicsJoint = CalculateJointBetween(pointA, pointB);
-            SetJoint(currentJoint.ValueRO, physicsJoint, ref state);
+        // Get the input state singleton
+        var inputState = SystemAPI.GetSingleton<InputSystem.InputState>();
+        if (inputState.Q.IsSet(SystemAPI.GetSingleton<FixedTickSystem.Singleton>().Tick)) {
+            foreach (var (followPoints, localTransform, vel, currentJoint) in SystemAPI.Query<DynamicBuffer<FollowPoint>, RefRO<LocalTransform>, RefRO<PhysicsVelocity>, RefRO<CurrentJoint>>()) {
+                // get the first two points
+                float3 pointA = followPoints[i];
+                float3 pointB = followPoints[i+1];
+                var physicsJoint = JointHelpers.CalculateJointBetween(pointA, pointB);
+                SystemAPI.SetComponent(currentJoint.ValueRO.Value, physicsJoint);
+                i++;
+                if (i >= followPoints.Length - 1) i = 0;
+            }
         }
     }
+}
 
+public static class JointHelpers {
     public static Entity CreateJoint(EntityCommandBuffer ecb, Entity entity, PhysicsJoint physicsJoint){
-            var constraintBodyPair = new PhysicsConstrainedBodyPair(entity, Entity.Null, false);
-            var jointEntity = ecb.CreateEntity();
-            ecb.AddSharedComponent(jointEntity, new PhysicsWorldIndex(0));
-            ecb.AddComponent(jointEntity, constraintBodyPair);
-            ecb.AddComponent(jointEntity, physicsJoint);
-            return jointEntity;
+        var constraintBodyPair = new PhysicsConstrainedBodyPair(entity, Entity.Null, false);
+        var jointEntity = ecb.CreateEntity();
+        ecb.AddSharedComponent(jointEntity, new PhysicsWorldIndex(0));
+        ecb.AddComponent(jointEntity, constraintBodyPair);
+        ecb.AddComponent(jointEntity, physicsJoint);
+        return jointEntity; 
     }
 
-    void SetJoint(CurrentJoint currentJoint, PhysicsJoint physicsJoint, ref SystemState state){
-        var jointEntity = currentJoint.Value;
-        SystemAPI.SetComponent(jointEntity, physicsJoint);
+    public static Entity CreateJoint<T>(Baker<T> baker, Entity entity, PhysicsJoint physicsJoint) where T : Component {
+        var constraintBodyPair = new PhysicsConstrainedBodyPair(entity, Entity.Null, false);
+        var jointEntity = baker.CreateAdditionalEntity();
+        baker.AddSharedComponent(jointEntity, new PhysicsWorldIndex(0));
+        baker.AddComponent(jointEntity, constraintBodyPair);
+        baker.AddComponent(jointEntity, physicsJoint);
+        return jointEntity; 
     }
 
     public static PhysicsJoint CalculateJointBetween(float3 pointA, float3 pointB) {
-            // get the direction from pointA to pointB
-            var direction = pointB - pointA;
-            var distance = math.length(direction);
-            var body = new BodyFrame 
-            {
-                Axis = math.normalize(direction),
-                PerpendicularAxis = 1,
-            };
-            var bodyB = body;
-            bodyB.Position = pointA;
-            var physicsJoint = PhysicsJoint.CreatePrismatic(
-                body, bodyB,
-                new float2(0, distance)
-            );
+        // get the direction from pointA to pointB
+        var direction = pointB - pointA;
+        var distance = math.length(direction);
+        var body = new BodyFrame 
+        {
+            Axis = math.normalize(direction),
+            PerpendicularAxis = 1,
+        };
+        var bodyB = body;
+        bodyB.Position = pointA;
+        var physicsJoint = PhysicsJoint.CreatePrismatic(
+            body, bodyB,
+            new float2(0, distance)
+        );
 
-            physicsJoint.SetImpulseEventThresholdAllConstraints(math.INFINITY);
+        physicsJoint.SetImpulseEventThresholdAllConstraints(math.INFINITY);
 
-            return physicsJoint;
+        return physicsJoint;
     }
 }
